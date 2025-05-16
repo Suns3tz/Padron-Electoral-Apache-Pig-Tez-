3b83ee0e0bd2fc743e1b5c4e9f6a3f97ac14c0d3
William Jiménez <williamjimv668@gmail.com>
2025-05-15 21:18:11 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import './shared/App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchIdentification from './searchID/SearchIdentification.jsx';
-//import Distrito from './searchID/Distrito.jsx'; <Route path="/distrito" element={<Distrito />} />
+import Distrito from './searchID/Distrito.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<SearchIdentification />} />
-        
+        <Route path="/distrito" element={<Distrito />} />
       </Routes>
     </Router>
   );
